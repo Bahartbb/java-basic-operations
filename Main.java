a47d92107c6de703b4aafde600a06a45685647bd
Bahar <bahar.tbb@gamil.com>
2026-04-01 16:08:31 +0200

    Add MathUtils and Main classes

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(MathUtils.add(10, 5));
+        System.out.println(MathUtils.increment(10));
+        System.out.println(MathUtils.decrement(10));
+        System.out.println(MathUtils.negate(10));
+
+    }
+}
---
+++ MathUtils.java
@@ -0,0 +1,18 @@
+public class MathUtils {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int increment(int number) {
+        return number + 1;
+    }
+
+    public static int decrement(int number) {
+        return number - 1;
+    }
+
+    public static int negate(int number) {
+        return -number;
+    }
+}
